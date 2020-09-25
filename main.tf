dec27507233c83615d8ea38dbfab0f75c9ff9857
abandari2 <abhishekbandari460@gmail.com>
2020-09-25 16:37:44 +0530

    updates

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "techlanders-statefile"
-    key  = "terraform/state"
-    region = "us-east-2"
+    bucket  = "jaslin-s3"
+    key  = "cd/state"
+    region = "us-east-1"
 #   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
 #   secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
   }
@@ -12,14 +12,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "myawsserver" {
-  ami = "ami-0b16724fe8e66e4ec"
-  key_name = "gagan-cicd"
+resource "aws_instance" "jaslinawsserver" {
+  ami = "ami-0ec2192bf0ab56546"
+  key_name = "lastday-jaslin"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Gagan-Ubuntu-Server"
-    Env = "Dev"
+    Name = "Jaslin-Ubuntu-Server"
+    Env = "Prod"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.private_ip} myawsserver >> /etc/hosts"
@@ -27,13 +27,15 @@ resource "aws_instance" "myawsserver" {
  
 provisioner "remote-exec" {
     inline = [
-     "touch /tmp/gagandeep"
+     "touch /tmp/jaslin"
+     "touch /tmp/abhi"
+     "touch /tmp/ankita"
      ]
  connection {
     type     = "ssh"
     user     = "ubuntu"
     insecure = "true"
-    private_key = "${file("/tmp/gagan-cicd.pem")}"
+    private_key = "${file("/tmp/lastday-jaslin.pem")}"
     host     =  aws_instance.myawsserver.public_ip
   }
 }
